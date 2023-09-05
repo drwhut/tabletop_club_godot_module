9695edf45f23e34813371f9f18406dd48f633e40
drwhut <drwhut@gmail.com>
2023-09-06 00:13:34 +0100

    Fixed ErrorReporter not preserving UTF-8 encoded strings.
    So, as per usual, it wasn't the engine's fault, it was mine. Again.
    
    In attempting to fix drwhut/tabletop-club#291, I had thought there
    was a flaw in how the engine handled errors and warnings, since the
    error message was converted from a Godot string to a C string, I
    thought that any UTF-8 encoding was lost in that conversion. I was
    wrong - the data was still there, but when it reached the
    ErrorReporter I had used the default String() constructor, not the
    static String::utf8() function, which handles UTF-8 strings.
    
    AAAAAAAAAAAAAAAAA

--- error_reporter.cpp
+++ error_reporter.cpp
@@ -41,10 +41,10 @@ void ErrorReporter::_bind_methods() {
 void ErrorReporter::_error_handler(void *p_self, const char *p_func, const char *p_file, int p_line, const char *p_error, const char *p_errorexp, ErrorHandlerType p_type) {
     ErrorReporter *self = (ErrorReporter *)p_self;
 
-    String func_str = String(p_func);
-    String file_str = String(p_file);
-    String error_str = String(p_error);
-    String errorexp_str = String(p_errorexp);
+    String func_str = String::utf8(p_func);
+    String file_str = String::utf8(p_file);
+    String error_str = String::utf8(p_error);
+    String errorexp_str = String::utf8(p_errorexp);
 
     if (p_type == ERR_HANDLER_WARNING) {
         self->emit_signal("warning_received", func_str, file_str, p_line, error_str, errorexp_str);
